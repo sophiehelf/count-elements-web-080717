5db1b9822aad63e790a7121d8a3e36e0cf32a5ac
Sophie Helf <sophie.helf@gmail.com>
2017-08-08 01:19:18 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |element|
+  	if !new_hash.has_key?(element)
+  		new_hash[element] = 1
+  	else
+  		new_hash[element] +=1
+  	end
+  end
+  new_hash
 end
  
